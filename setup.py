6b6f08a0edb32a03f062e60610ec5fde758481da
Kirk Byers <ktbyers@twb-tech.com>
2015-07-05 11:16:37 -0700

    pypi packaging

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
-import netmiko
-
 setup(
     name='netmiko',
-    version=netmiko.__version__,
+    version='0.2.1',
     url='https://github.com/ktbyers/netmiko',
+    download_url='https://github.com/ktbyers/netmiko/archive/v0.2.0.tar.gz',
     license='MIT',
     author='Kirk Byers',
-    install_requires=['paramiko>=1.7.5', 'scp>=0.10.0'],
+    author_email='ktbyers@twb-tech.com',
+    install_requires=['paramiko>=1.13.0', 'scp>=0.10.0'],
     description='Multi-vendor library to simplify Paramiko SSH connections to network devices',
     packages=['netmiko',
               'netmiko/cisco',
